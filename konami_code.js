309284a8ffdbf0c4d9559ee0e03c785d64b438db
Raymond Chow <raychow1944@yahoo.com>
2018-11-17 23:38:19 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,20 +11,20 @@ const codes = [
   "a"
 ];
 
-// function init() {
-//   // your code here
-//   let i = 0;
-//   document.body.addEventListener('keydown', function(e) {
-//     const key = e.key;
-//     if(key === codes[i]) {
-//       i++;
-//       if(i === codes.length) {
-//         alert("Hurray!");
+function init() {
+  // your code here
+  let i = 0;
+  document.body.addEventListener('keydown', function(e) {
+    const key = e.key;
+    if(key === codes[i]) {
+      i++;
+      if(i === codes.length) {
+        alert("Hurray!");
         
-//         i = 0;
-//       }
-//     } else {
-//       i = 0;
-//     }
-//   })
-// }
+        i = 0;
+      }
+    } else {
+      i = 0;
+    }
+  })
+}
